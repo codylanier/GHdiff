eef984028c29efd3f9a52a23879c28d5d10f0f9f
Cody Lanier <cody.lanier@duke.edu>
2019-12-19 09:44:17 -0500

    minor changes

--- src/method.py
+++ src/method.py
@@ -6,7 +6,6 @@ Created on Tue Dec 17 15:44:09 2019
 """
 
 import chparse
-
-with open('lostdream.chart') as chartfile:
-    chart = chparse.load(chartfile)
+with open('notes.chart') as chartfile:
+    chart = chparse.parse.load(chartfile)
 print(chart.instruments[chparse.EXPERT][chparse.GUITAR][0])
