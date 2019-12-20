3b3d373e7eee54f9d25ed87ac73c29fdb202d6ca
Cody Lanier <cody.lanier@duke.edu>
2019-12-19 20:24:37 -0500

    fixed chart import, added dictionary of BPM events, added dictionary of notes

--- src/method.py
+++ src/method.py
@@ -5,7 +5,39 @@ Created on Tue Dec 17 15:44:09 2019
 @author: codyl
 """
 
+def tickstoseconds(ticks, bpm):
+    return None
+
+#%% import chart
 import chparse
-with open('notes.chart') as chartfile:
+with open('notes.chart', encoding=('utf-8-sig')) as chartfile:
     chart = chparse.parse.load(chartfile)
-print(chart.instruments[chparse.EXPERT][chparse.GUITAR][0])
+xg = chart.instruments[chparse.EXPERT][chparse.GUITAR]
+chartlength = len(xg)
+#print(xg)
+
+#%% get dictionary of BPM events and tick numbers
+sync = chart.sync_track
+bpmarray = {}
+for bpm in sync:
+    if bpm.kind.value == 'B':
+        if bpmarray.get(bpm.time, -1) == -1:
+            bpmarray[bpm.time] = [bpm.value / 1000]
+        else:
+            bpmarray[bpm.time] += [bpm.value / 1000] #catch all just in case, this should be unused though
+            
+#%% add notes to a dictionary cooresponding to their tick number, and group chords together
+fretarray = {}
+for note in xg:
+    if note.kind.value == 'N':
+        if fretarray.get(note.time, -1) == -1:
+            fretarray[note.time] = [note.fret]
+        else:
+            fretarray[note.time] += [note.fret]
+
+#%%calcs
+
+
+#%% printing diagnostincs
+print(fretarray)
+print(bpmarray)
